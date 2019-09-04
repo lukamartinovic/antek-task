02a26bbfaf13d5f5046e8ed8edd1a18dba10899e
Luka <lukamartinovic@gmail.com>
2019-09-04 17:58:08 +0200

    done

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,10 @@
-import axios from 'axios';
-
-const routes = {
-    getAll: '/get',
-    getId: '/get/',
-    addEntry: '/post',
-    deleteEntry: '/delete/',
-    editEntry: '/put'
-};
-
-const host = 'localhost:3000';
-
-export const requests = {
-    getAll(callback, errorCallback) {
-        axios.get(host + routes.getAll)
-            .then(res => callback(res))
-            .catch(err => callback(err))
-    }
+export default {
+    routes: {
+        getAll: '/get',
+        getId: '/get/',
+        addEntry: '/post',
+        deleteEntry: '/delete/',
+        editEntry: '/put/'
+    },
+    host: "http://localhost:3000"
 }
